237fc4aa60e45c75d23e2b873d9b0e3e245f6474
Victor Falck-Næss <victor.falcknaess@gmail.com>
2018-09-21 13:33:41 +0100

    Start

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Shoot", 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Shoot() {
+        Instantiate(bullet, new Vector3(1, 0, 0), Quaternion.identity);
+    }
+}
