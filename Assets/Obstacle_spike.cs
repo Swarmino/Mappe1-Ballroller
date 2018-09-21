054408ed77bfecfffc11f28fe6dd00420b1dfc93
Victor Falck-Næss <victor.falcknaess@gmail.com>
2018-09-21 14:59:48 +0100

    update

---
+++ Assets/Obstacle_spike.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_spike : MonoBehaviour {
+
+    if (object.Player)
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+}
