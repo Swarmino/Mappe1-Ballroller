1dbd8848dbd32f2ace53f44becdc60cd679a1813
Victor Falck-Næss <victor.falcknaess@gmail.com>
2018-09-27 15:17:17 +0100

    level1 - update

---
+++ Assets/Scripts/HazardIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HazardIdentifier : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hazard")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
